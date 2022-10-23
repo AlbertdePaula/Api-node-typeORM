e9c7fcf8140da2ce8526ca17d35dfcee9227adc2
Albert_Remote <albert_depaula@yahoo.com.br>
2022-10-22 22:35:16 -0300

    implementado o arquivo de rota engine.routes

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,13 @@ import express from "express";
 import morgan from 'morgan';
 import cors from 'cors';
 
+import engineRoutes from "./routes/engine.routes";
+
 const app = express();
 
 app.use(morgan('dev'));
 app.use(cors());
+
+app.use(engineRoutes);
 
 export default app;
